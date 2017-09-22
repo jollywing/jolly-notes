a8e19639e9e7c9070b086eee1034823621ddf1b0
Jolly Wing <jollywing@foxmail.com>
2017-09-22 15:32:03 +0800

    new notes archive

---
+++ mindmaps/征求意见稿反馈意见.mm
@@ -0,0 +1,27 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1469514957502" ID="ID_1347394443" MODIFIED="1469514980167" TEXT="&#x5f81;&#x6c42;&#x610f;&#x89c1;&#x7a3f;&#x53cd;&#x9988;&#x610f;&#x89c1;">
+<font BOLD="true" NAME="SansSerif" SIZE="16"/>
+<node CREATED="1469515144207" ID="ID_536876373" MODIFIED="1469515153437" POSITION="right" TEXT="&#x5b89;&#x5168;&#x5ba1;&#x8ba1;">
+<font NAME="SansSerif" SIZE="15"/>
+<node CREATED="1469515157320" ID="ID_1686655310" MODIFIED="1469515173923" TEXT="&#x5ba1;&#x8ba1;&#x65e5;&#x5fd7;&#x63a5;&#x53e3;">
+<font NAME="SansSerif" SIZE="13"/>
+<node CREATED="1469515177580" ID="ID_492545083" MODIFIED="1469515180736" TEXT="syslog"/>
+<node CREATED="1469515181396" ID="ID_598661381" MODIFIED="1469515184963" TEXT="webservice"/>
+</node>
+</node>
+<node CREATED="1469515234241" ID="ID_1212112864" MODIFIED="1469515246231" POSITION="left" TEXT="&#x865a;&#x62df;&#x673a;&#x5bc6;&#x7ea7;&#x6807;&#x5fd7;">
+<font NAME="SansSerif" SIZE="15"/>
+<node CREATED="1469515253833" ID="ID_643490156" MODIFIED="1469515263996" TEXT="&#x54ea;&#x4e9b;&#x5bc6;&#x7ea7;">
+<font NAME="SansSerif" SIZE="13"/>
+</node>
+<node CREATED="1469515282789" ID="ID_532931471" MODIFIED="1469515293524" TEXT="&#x5bc6;&#x7ea7;&#x5bf9;&#x8bbf;&#x95ee;&#x7684;&#x5f71;&#x54cd;">
+<font NAME="SansSerif" SIZE="13"/>
+</node>
+</node>
+<node CREATED="1469515491799" ID="ID_1830227188" MODIFIED="1469515515511" POSITION="right" TEXT="&#x865a;&#x62df;&#x673a;&#x4ea7;&#x54c1;&#x7ec4;&#x4ef6;">
+<font NAME="SansSerif" SIZE="14"/>
+<node CREATED="1469515518801" ID="ID_1398964019" MODIFIED="1469515527199" TEXT="&#x5305;&#x62ec;&#x4ec0;&#x4e48;&#xff1f;"/>
+</node>
+</node>
+</map>
